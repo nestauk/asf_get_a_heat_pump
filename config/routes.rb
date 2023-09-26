1ebc6ef7460d43a0f7dbd0920697fee39c7587c5
Suraj Vadgama <suninthesky@users.noreply.github.com>
2023-09-26 15:19:53 +0100

    Add extra steps pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   get '/my-journey', to: 'journey#show', as: 'journey'
 
+  get '/step-2', to: 'pages#step2', as: 'step2'
   get '/step-3', to: 'pages#step3', as: 'step3'
+  get '/step-4', to: 'pages#step4', as: 'step4'
 
   post "/webhooks/new-response", to: "webhooks#new_response", as: "webhooks_new_response"
   get  "/webhooks/redirect/:token", to: "webhooks#redirect"
